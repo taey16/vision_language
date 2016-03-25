3618f5be9a6c008c57fa5e71788b98f9f107d8b9
taey16 <taey1600@gmail.com>
2016-03-25 16:07:05 +0900

    Commit

--- data/attribute/prepro_attribute.py
+++ data/attribute/prepro_attribute.py
@@ -271,14 +271,17 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser()
 
   parser.add_argument('--input_filename', default= \
-    '/storage/freebee/tshirts_shirts_blous_knit_jacket_onepiece.image_sentence.txt.shuffle.txt',
+    '/storage/freebee/tshirts_shirts_blous_knit_jacket_onepiece_skirts_coat_cardigan_vest.image_sentence.txt.shuffle.txt',
+    #'/storage/freebee/tshirts_shirts_blous_knit_jacket_onepiece_coat_skirt_cardigan_vest.image_sentence.txt.shuffle.txt',
+    #'/storage/freebee/tshirts_shirts_blous_knit_jacket_onepiece.image_sentence.txt.shuffle.txt',
     #'/storage/freebee/tshirts_shirts_blous_knit.image_sentence.txt.shuffle.txt',
     #'/storage/freebee/tshirts_shirts_blous.image_sentence.txt',
     #'/storage/freebee/tshirts_shirts.image_sentence.txt',
     #'/storage/freebee/csv_backup/tshirts_excel_1453264869210.csv.image_sentence.txt',
     help='number of images to assign to validation data (for CV etc)')
   parser.add_argument('--num_val', default= \
-    8000,
+    50000,
+    #8000,
     #6400,
     #6000,
     #5000,
@@ -286,7 +289,8 @@ if __name__ == "__main__":
     type=int, 
     help='number of images to assign to validation data (for CV etc)')
   parser.add_argument('--num_test', default= \
-    8000,
+    50000,
+    #8000,
     #6400,
     #6000,
     #5000,
@@ -294,14 +298,18 @@ if __name__ == "__main__":
     type=int, 
     help='number of images to assign to tesst data (for CV etc)')
   parser.add_argument('--output_json', default= \
-    '/storage/freebee/tshirts_shirts_blous_knit_jacket_onepiece.image_sentence.txt.shuffle.txt.cutoff100.json',
+    '/storage/freebee/tshirts_shirts_blous_knit_jacket_onepiece_skirts_coat_cardigan_vest.image_sentence.txt.shuffle.txt.cutoff100.json',
+    #'/storage/freebee/tshirts_shirts_blous_knit_jacket_onepiece_coat_skirt_cardigan_vest.image_sentence.txt.shuffle.txt.cutoff100.json',
+    #'/storage/freebee/tshirts_shirts_blous_knit_jacket_onepiece.image_sentence.txt.shuffle.txt.cutoff100.json',
     #'/storage/freebee/tshirts_shirts_blous_knit.image_sentence.txt.shuffle.txt.cutoff100.json', 
     #'/storage/freebee/tshirts_shirts_blous.image_sentence.txt.json', 
     #'/storage/freebee/tshirts_shirts.image_sentence.txt.json',
     #'/storage/freebee/tshirts.image_sentence.txt.json',
     help='output json file')
   parser.add_argument('--output_h5', default= \
-    '/storage/freebee/tshirts_shirts_blous_knit_jacket_onepiece.image_sentence.txt.shuffle.txt.cutoff100.h5',
+    '/storage/freebee/tshirts_shirts_blous_knit_jacket_onepiece_skirts_coat_cardigan_vest.image_sentence.txt.shuffle.txt.cutoff100.h5',
+    #'/storage/freebee/tshirts_shirts_blous_knit_jacket_onepiece_coat_skirt_cardigan_vest.image_sentence.txt.shuffle.txt.cutoff100.h5',
+    #'/storage/freebee/tshirts_shirts_blous_knit_jacket_onepiece.image_sentence.txt.shuffle.txt.cutoff100.h5',
     #'/storage/freebee/tshirts_shirts_blous_knit.image_sentence.txt.shuffle.txt.cutoff100.h5', 
     #'/storage/freebee/tshirts_shirts_blous.image_sentence.txt.h5', 
     #'/storage/freebee/tshirts_shirts.image_sentence.txt.h5',
