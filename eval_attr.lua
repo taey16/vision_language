f99d38836a2dea6d28cabefd11fe1b526070687f
taey16 <taey16@sk.com>
2016-09-19 14:38:32 +0900

    remove path

--- eval_attr.lua
+++ eval_attr.lua
@@ -20,7 +20,7 @@ require 'models.FeatExpander'
 local net_utils = require 'misc.net_utils'
 
 local model_filename = 
-  '/storage/attribute/checkpoints/tshirts_shirts_blous_knit_jacket_onepiece_skirts_coat_cardigan_vest_pants_leggings_shoes_bags_swimwears_hat_panties_bra_801544_40000_seq_length14/resception_ep29_bs16_flipfalse_croptrue_original_init_gamma0.100000_lstm_tanh_hid512_lay2_drop2.000000e-01_adam_lr1.000000e-03_seed0.90_start541152_every45096_finetune0_cnnlr1.000000e-03_cnnwc1.000000e-05_retrain_iter0/model_idresception_ep29_bs16_flipfalse_croptrue_original_init_gamma0.100000_lstm_tanh_hid512_lay2_drop2.000000e-01_adam_lr1.000000e-03_seed0.90_start541152_every45096_finetune0_cnnlr1.000000e-03_cnnwc1.000000e-05_retrain_iter0.t7'
+  '*.t7'
 local image_size = 342
 local crop_size = 299
 local seq_length = 14
